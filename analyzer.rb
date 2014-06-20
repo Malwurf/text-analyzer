cdec2867f35eb4f72dae67f9eeea52b850caa764
Malwurf <pk28@o2.pl>
2014-06-20 14:09:15 +0200

    kod programu

--- analyzer.rb
+++ analyzer.rb
@@ -1,3 +1,7 @@
+text = ''
 line_count = 0
-File.open("text.txt").each {|line| line_count +=1}
-puts line_count
+File.open("text.txt").each do |line|
+  line_count +=1
+  text << line
+end
+puts "Liczba wierszy w tekście - #{line_count}"
